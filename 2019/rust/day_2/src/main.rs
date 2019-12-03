a65107bf765f6348f132bd1f7ad7fcefbeb26ad9
pmkroeker <peter.kroeker@metrixplanttech.com>
2019-12-03 10:44:09 -0800

    Solve part 2

--- 2019/rust/day_2/src/main.rs
+++ 2019/rust/day_2/src/main.rs
@@ -17,7 +17,7 @@ use std::process::exit;
 fn main() {
     // Open file
     let intcode_file = parse_file("input.txt");
-    let mut intcode = match intcode_file {
+    let intcode = match intcode_file {
         Ok(v) => v,
         Err(e) => {
             println!("Error parsing file: {:?}", e);
@@ -25,34 +25,48 @@ fn main() {
         }
     };
     // Perform operations!
-    let mut current_position: usize = 0;
     let code_length = intcode.len();
     // Problem stats to changes theses values!
-    intcode[1] = 12;
-    intcode[2] = 2;
-    loop {
-        let opcode = intcode[current_position];
-        // println!("{}\n", current_position);
-        if opcode == 99 || current_position >= code_length - 1 {
-            break;
-        }
-        let pos_1: usize = current_position + 1;
-        let pos_2: usize = current_position + 2;
-        let pos_3: usize = current_position + 3;
+    // Part 2 these are dynamic to produce a final result of 19690720
+    // Will be between 0 & 99
+    let mut final_noun = 0;
+    let mut final_verb = 0;
+    'noun: for noun in 00..100 {
+        for verb in 0..100 {
+            let mut mut_intcode = intcode.clone();
+            let mut current_position: usize = 0;
+            mut_intcode[1] = noun;
+            mut_intcode[2] = verb;
+            loop {
+                let opcode = mut_intcode[current_position];
+                // println!("{}\n", current_position);
+                if opcode == 99 || current_position >= code_length - 1 {
+                    break;
+                }
+                let param_1: usize = current_position + 1;
+                let param_2: usize = current_position + 2;
+                let param_3: usize = current_position + 3;
 
-        let pos_first: usize = intcode[pos_1].try_into().unwrap();
-        let pos_second: usize = intcode[pos_2].try_into().unwrap();
-        let res_pos: usize = intcode[pos_3].try_into().unwrap();
-        let first_val = intcode[pos_first];
-        let second_val = intcode[pos_second];
-        if opcode == 1 {
-            intcode[res_pos] = first_val + second_val;
-        } else if opcode == 2 {
-            intcode[res_pos] = first_val * second_val;
+                let pos_first: usize = mut_intcode[param_1].try_into().unwrap();
+                let pos_second: usize = mut_intcode[param_2].try_into().unwrap();
+                let res_pos: usize = mut_intcode[param_3].try_into().unwrap();
+                let first_val = mut_intcode[pos_first];
+                let second_val = mut_intcode[pos_second];
+                if opcode == 1 {
+                    mut_intcode[res_pos] = first_val + second_val;
+                } else if opcode == 2 {
+                    mut_intcode[res_pos] = first_val * second_val;
+                }
+                current_position += 4;
+            }
+            if mut_intcode[0] == 19_690_720 {
+                final_noun = noun;
+                final_verb = verb;
+                break 'noun;
+            }
         }
-        current_position += 4;
     }
-    println!("{}", intcode[0]);
+    println!("Noun:{} | Verb:{}", final_noun, final_verb);
 }
 
 /// Parses file and returns comma separated int list as a Vector of u16
