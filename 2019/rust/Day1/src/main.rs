2779b4a07b55c3212d8040b678e8d6e39b8da315
pmkroeker <peter.kroeker@metrixplanttech.com>
2019-12-03 08:39:11 -0800

    Day 1 Part 1 Complete

--- 2019/rust/Day1/src/main.rs
+++ 2019/rust/Day1/src/main.rs
@@ -1,9 +1,27 @@
-use std::env;
-use std::fs;
+use std::fs::File;
+use std::str::FromStr;
+use std::io::{prelude::BufRead, BufReader};
+use std::f32;
 
-fn main() {
-    let contents = fs::read_to_string("input.txt")
-        .expect("Something when wrong reading the file.");
+fn main() -> std::io::Result<()> {
+    let file = File::open("input.txt").unwrap();
+    let reader = BufReader::new(file);
+    let mut total_fuel: f32 = 0.0;
+    for line in reader.lines() {
+        let value = line.unwrap();
+        let immut_value = &value;
+        let to_parse: &str = &immut_value;
+        let num = f32::from_str(to_parse).unwrap();
+        let fuel = calc_fuel(num);
+        total_fuel += fuel;
+    }
+    println!("{}", total_fuel);
+    Ok(())
+}
 
-    println!("With text:\n{}", contents);
+/// Fuel required is mass / 3 rounded down then subtract 2
+fn calc_fuel(mass: f32) -> f32 {
+    let halved = mass / 3.0;
+    let floored = halved.floor();
+    floored - 2.0
 }
